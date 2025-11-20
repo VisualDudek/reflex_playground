09d5056f6cb46c437ff302fd439e37c6941eed75
MarcinDudek <dudek.v@gmail.com>
2025-11-20 16:07:19 +0100

    add state management for label changes in the app

--- start_here/start_here/start_here.py
+++ start_here/start_here/start_here.py
@@ -3,10 +3,21 @@
 import reflex as rx
 
 from rxconfig import config
+from itertools import cycle
 
 
 class State(rx.State):
     """The app state."""
+    label: str = "Hello, Reflex!"
+    _label_cycle = cycle(["Hello, Reflex!", "You clicked the button!"])
+
+    def change_label(self):
+        """Change the label."""
+        self.label = "You clicked the button!"
+
+    def toggle_label(self):
+        """Toggle the label between two states."""
+        self.label = next(self._label_cycle)
 
 
 def index() -> rx.Component:
@@ -15,10 +26,19 @@ def index() -> rx.Component:
         rx.color_mode.button(position="top-right"),
         rx.vstack(
             rx.heading("Welcome to Reflex!", size="9"),
+            rx.heading(State.label, size="9"),
             rx.text(
                 "Get started by editing ",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
                 size="5",
+            ),
+            rx.button(
+                "Click me!",
+                on_click=State.change_label,
+            ),
+            rx.button(
+                "Toggle Label",
+                on_click=State.toggle_label,
             ),
             rx.link(
                 rx.button("Check out our docs!"),
