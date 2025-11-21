f84d09757612d40e8f26f2feee256bdf356d9ef8
MarcinDudek <dudek.v@gmail.com>
2025-11-21 11:21:40 +0100

    enhance input handling in README and implement input change handling in State class

--- html_input_001/README.md
+++ html_input_001/README.md
@@ -1,4 +1,10 @@
 # html input for dynamic input
 
+- event vs. event handlers, the first one is on frontend/user UI space and the "event hadlers" refers to backend
+
 ## Props
-- there is also `on_change` evnet
+- there is also `on_change` event, will be called on every change e.g. single chr
+
+## Input
+- `rx.input()` works with `on_change` props
+- can have `placeholder` or `default_value`
--- html_input_001/html_input_001/html_input_001.py
+++ html_input_001/html_input_001/html_input_001.py
@@ -9,6 +9,10 @@ class State(rx.State):
     """The app state."""
     label: str = "Hello, Reflex!"
 
+    def handle_input(self, value: str) -> None:
+        """Handle input change."""
+        self.label = value
+
 
 def index() -> rx.Component:
     # Welcome Page (Index)
@@ -21,6 +25,7 @@ def index() -> rx.Component:
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
                 size="5",
             ),
+            rx.input(on_change=State.handle_input, placeholder="Type something..."),
             rx.link(
                 rx.button("Check out our docs!"),
                 href="https://reflex.dev/docs/getting-started/introduction/",
